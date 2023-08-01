3acf70426ec8aca9bf95d9841b7a9e68b8c81e01
Akash Kumar <akashkumar59999@gmail.com>
2023-08-01 10:22:27 +0530

    done with it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import "./App.css";
-import Card from "./components/Card";
-import AddUser from "./components/AddUser/AddUser";
-import UserList from "./components/UsersList/UsersList";
+// import Card from "./components/UI/Card";
+// import AddUser from "./components/Users/AddUser";
+// import UserList from "./components/UsersList/UsersList";
 import { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UsersList";
 
 function App() {
-  const [userData, setUserData] = useState([]);
+  const [usersList,setUsersList] = useState([]);
 
-  const addDataHandler = (userInput) => {
-    setUserData((prevData) => ([...prevData,{...userInput,id: Math.random().toString()}]));
-    console.log(userData);
-  };
-
+  const addUserHandler = (uName,uAge) => {
+    setUsersList((prevState) => {
+      return [...prevState,{name:uName,age:uAge}]
+    })
+  }
   return (
-    <div className="App">
-      <Card>
-        <AddUser onAddData={addDataHandler} />
-      </Card>
-      <Card>
-        <UserList list={userData}/>
-      </Card>
+    <div>
+      <AddUser onAddUser={addUserHandler}/>
+      <UserList users={usersList}/>
     </div>
   );
 }
